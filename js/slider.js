f22eae527baf76939588f23cb8c0dfe7ab18af70
minhsang1311 <109707462+minhsang1311@users.noreply.github.com>
2022-12-14 00:28:52 +0700

    final

--- js/slider.js
+++ js/slider.js
@@ -10,7 +10,7 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
-        600:{
+        1024:{
             items:2
         },
     }
@@ -38,21 +38,4 @@ $(document).ready(function() {
 
     });
 
-});
-
-/* color book */
-    $(document).ready(function(){
-    $(".group3").colorbox({rel:'group3', transition:"none", width:"75%", height:"75%"});
-    $(".callbacks").colorbox({
-    onOpen:function(){ alert('onOpen: colorbox is about to open'); },
-    onLoad:function(){ alert('onLoad: colorbox has started to load the targeted content'); },
-    onComplete:function(){ alert('onComplete: colorbox has displayed the loaded content'); },
-    onCleanup:function(){ alert('onCleanup: colorbox has begun the close process'); },
-    onClosed:function(){ alert('onClosed: colorbox has completely closed'); }
-});
-    //Example of preserving a JavaScript event for inline calls.
-    $("#click").click(function(){
-    $('#click').css({"background-color":"#f00", "color":"#fff", "cursor":"inherit"}).text("Open this window again and this message will still be here.");
-    return false;
-});
 });
